24469a9c4f2eecf8d98641dc43040e1c13603ef7
alonso329 <asepulveda1@ucol.mx>
2021-01-17 12:36:52 -0600

    LoginHome accesible listo

--- screens/LoginHome.js
+++ screens/LoginHome.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 if (Text.defaultProps == null) Text.defaultProps = {};
 Text.defaultProps.allowFontScaling = false;
@@ -8,24 +8,38 @@ Text.defaultProps.allowFontScaling = false;
 const LoginHome = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.whiteBox}>
-        <View style={styles.icon}>
-          <FontAwesome name="hand-peace-o" size={200} color="black" />
-          <Text style={styles.iconText}>Eventos Colima</Text>
+      <View style={styles.whiteBox} accessible={true}>
+        <View style={styles.icon} accessible={true}>
+          <FontAwesome
+            name="hand-peace-o"
+            size={200}
+            color="black"
+            accessibilityLabel="Icono de eventos colima"
+            accessibilityRole="image"
+          />
+          <Text style={styles.iconText} accessibilityRole="header">
+            Eventos Colima
+          </Text>
         </View>
         <TouchableOpacity
           onPress={() => {
-            navigation.push('Register');
+            navigation.push("Register");
           }}
           style={styles.button}
+          accessibilityRole="button"
+          accessibilityLabel="Registrarse"
+          accessibilityHint="Ir a la pagina de registro"
         >
           <Text style={styles.buttonText}>REGISTRARSE</Text>
         </TouchableOpacity>
-        <View style={styles.button}>
+        <View style={styles.button} accessible={true}>
           <TouchableOpacity
             onPress={() => {
-              navigation.push('LoginForm');
+              navigation.push("LoginForm");
             }}
+            accessibilityRole="button"
+            accessibilityLabel="Iniciar Sesión"
+            accessibilityHint="Ir a la pagina de inicio de sesion"
           >
             <Text style={styles.buttonText}>INICIAR SESIÓN</Text>
           </TouchableOpacity>
@@ -34,9 +48,12 @@ const LoginHome = ({ navigation }) => {
           onPress={() => {
             navigation.reset({
               index: 0,
-              routes: [{ name: 'TabNavigator' }],
+              routes: [{ name: "TabNavigator" }],
             });
           }}
+          accessibilityRole="button"
+          accessibilityLabel="Ir a Home"
+          accessibilityHint="Ir a la pagina principal"
         >
           <Text>Ir a home</Text>
         </TouchableOpacity>
@@ -48,51 +65,51 @@ const LoginHome = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fcba03',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fcba03",
   },
   icon: {
     marginBottom: 40,
     marginHorizontal: 15,
   },
   button: {
-    backgroundColor: '#fcba03',
+    backgroundColor: "#fcba03",
     padding: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
+    alignSelf: "stretch",
+    justifyContent: "center",
     marginVertical: 12,
     borderRadius: 30,
     height: 50,
     elevation: 3,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
   },
   whiteBox: {
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
     margin: 30,
     padding: 50,
     borderRadius: 25,
-    maxWidth: '85%',
+    maxWidth: "85%",
     elevation: 3,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
   },
   iconText: {
     marginTop: 15,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 25,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   buttonText: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     fontSize: 14,
   },
 });
